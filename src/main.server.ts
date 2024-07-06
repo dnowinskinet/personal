002c4d6d7df3da677e7f83506b261c395d38e41f
dnowinskinet <github@dnowinski.com>
2024-07-06 18:42:16 -0400

    new

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
+import { App } from './app/app';
 import { appConfig } from './app/app.config';
 
-const bootstrap = () => bootstrapApplication(AppModule, appConfig);
+const bootstrap = () => bootstrapApplication(App, appConfig);
 
 export { AppServerModule as default } from './app/app.module.server';
