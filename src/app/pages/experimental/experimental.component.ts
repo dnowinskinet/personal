c0439a35e77c42a083fe31aa62cd3c4b11414987
dnowinskinet <github@dnowinski.com>
2024-07-07 15:49:05 -0400

    good so far

--- src/app/pages/experimental/experimental.component.ts
+++ src/app/pages/experimental/experimental.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, ViewEncapsulation, effect, inject } from '@angular/core';
+import { MetaService } from '@core/services/meta.service';
+import profileData from '@data/profile.data';
 
 @Component({
   selector: 'app-experimental',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './experimental.component.scss'
 })
 export class ExperimentalComponent {
+  metaService = inject(MetaService)
+  constructor(){
+    this.metaService.setMetaTags(
+      `About - ${profileData.name}`,
+      'Dive into a curated space to learn more about the person behind the name',
+      ['bio', 'biography', 'information', 'about', 'contact', 'detail']
+      )
+  }
 
 }
--- src/app/pages/home/expertise-area/intro.ts
+++ src/app/pages/home/expertise-area/intro.ts
@@ -12,6 +12,7 @@ import { Loader } from '@shared/components/loader/loader';
 @Component({
   selector: 'intro',
   template: `
+  @defer(on immediate){
     <section class="mt-8 relative">
       <div class="grid grid-cols-1">
         <div>
@@ -20,7 +21,7 @@ import { Loader } from '@shared/components/loader/loader';
         <div
           class="w-full sm:w-auto flex flex-col gap-3 justify-between sm:absolute top-0 left-0 sm:top-5 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-400 p-5">
           <p class="flex justify-start text-xl dark:text-gray-200 font-semibold"><span
-              class="animate-waving-hand">👋</span>&nbsp;&nbsp;!
+              class="animate-waving-hand">👋</span>&nbsp;&nbsp;{{ changingText() }}!
           </p>
           <h1 class="text-xl font-semibold dark:text-white">I'm <span class="text-primary-600 dark:text-primary-400 ">{{profile().name}}</span></h1>
           <span class="dark:text-gray-200">{{profile().bio}}</span>
@@ -35,7 +36,10 @@ import { Loader } from '@shared/components/loader/loader';
           </div>
         </div>
       </div>
-    </section>
+    </section>}
+    @placeholder {
+      <loader/>
+    }
   `,
   imports: [SocialLink, Button,ManSorrow, Icon, Loader],
   encapsulation: ViewEncapsulation.None,
@@ -44,5 +48,11 @@ import { Loader } from '@shared/components/loader/loader';
 })
 export class Intro {
   profile = signal<ProfileSchema>(ProfileData);
-  fileIcon = file
+  fileIcon = file;
+  public changingText = signal<string>(this.profile().greetings[0]);
+  platformCheck = Inject(PlatformCheckService)
+  cd = inject(ChangeDetectorRef)
+  currentGreetingIndex = signal<number>(0);
+  greetingSub!: Subscription;
+
 }
