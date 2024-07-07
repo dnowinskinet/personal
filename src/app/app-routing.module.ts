0fa2d4a45b474151bcfd1a44f9d03ec918c7e7dc
dnowinskinet <github@dnowinski.com>
2024-07-07 14:41:33 -0400

    error works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 //pages
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },  
-  { path: 'about', component: AboutComponent, pathMatch: 'full' },];
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
+  { path: '**', component: ErrorComponent, pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
